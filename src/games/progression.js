0f9464850d7699e60353ce7a6da91bd769c7f98a
ppbalyakin <ppbalyakin@domclick.ru>
2023-01-11 09:18:56 +0300

    fix error

--- src/games/progression.js
+++ src/games/progression.js
@@ -15,7 +15,7 @@ const progressionGame = () => {
   for (let i = 1; i <= attemptsCount; i += 1) {
     const progression = getProgression();
     const randomIdx = getRandomIntInclusive(0, progression.length - 1);
-    const rightAnswer = progression.splice(randomIdx, 1, '..');
+    const [rightAnswer] = progression.splice(randomIdx, 1, '..');
 
     askQuestion(`${progression.join(' ')}`);
     const answer = getAnswer();
